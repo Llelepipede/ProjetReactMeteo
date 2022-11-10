e3a4867aad865c9bf95e904e1eb690574d4b2fd9
Llelepipede <pchesneau3103@gmail.com>
2022-11-10 11:34:28 +0100

    testgeoloc

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Text, Image, TouchableOpacity, View} from 'react-native';
+
 import {Dimensions, SafeAreaView} from 'react-native';
 
-import Routes from './src/config/routes';
+// import Routes from './src/config/routes';
 
 const App = () => {
   return (
     <View>
-      <View></View>
+      <View />
     </View>
   );
 };
---
+++ src/components/Geoloc_Test/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text, Image, TouchableOpacity, View} from 'react-native';
+import styled from 'styled-components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// factorisation du code: on cree un componnent Button
+//    ->{onPress,label}: destructuration
+
+const Geoloc_Test = () => {};
