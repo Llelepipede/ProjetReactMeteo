329984057492d3630b1bfeb53dbd6d8621627358
Llelepipede <pchesneau3103@gmail.com>
2022-11-10 09:39:53 +0100

    premier commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Dimensions, SafeAreaView} from 'react-native';
+
+import Routes from './src/config/routes';
+
+const App = () => {
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+export default App;
