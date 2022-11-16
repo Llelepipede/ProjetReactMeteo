9540feaedc7c08a0236885ddb5f3829130a4378f
Delphine <delphine.pva@gmail.com>
2022-11-16 23:07:52 +0100

    New screen Prevision created, theme updated

--- src/configuration/routes.js
+++ src/configuration/routes.js
@@ -1,36 +1,24 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
 import BottomNavigator from './bottomNavigator';
 
-import Login from '../screens/Login';
 import Home from '../screens/Home';
+import Login from '../screens/Login';
+import Prevision from '../screens/Prevision';
 
 const Routes = props => {
-  // const Stack = createNativeStackNavigator();
   const RootStack = createStackNavigator();
 
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Home' }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginMeteo}
-          options={{ title: 'Login' }}
-        />
-      </Stack.Navigator> */}
       <RootStack.Navigator screenOptions={{ headerShown: false }} initialRouteName={'Login'}>
         <RootStack.Screen name="Login" component={Login} />
         <RootStack.Screen name="BottomNavigator" component={BottomNavigator} />
+        <RootStack.Screen name="Prevision" component={Prevision} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styled from 'styled-components';
 
 const Home = () => {
   const [datas, setDatas] = useState([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
+
     const getDatas = async () => {
       try {
         const result = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
@@ -25,23 +28,25 @@ const Home = () => {
   return (
     <View>
       <Text>Today</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Prevision')}>
+        <Text>7 days</Text>
+      </TouchableOpacity>
       <Container>
 
         <Text>T°C</Text>
-        <Test>
+        <Content>
+          <Box>
+            <Title>Wind</Title>
+          </Box>
 
-          <Content>
-            <Text>Wind</Text>
-          </Content>
+          <Box>
+            <Title>Humidity</Title>
+          </Box>
 
-          <Content>
-            <Text>Humidity</Text>
-          </Content>
-
-          <Content>
-            <Text>Chance of rain</Text>
-          </Content>
-        </Test>
+          <Box>
+            <Title>Chance of rain</Title>
+          </Box>
+        </Content>
 
       </Container>
       {/* {datas.map((data) => {
@@ -58,24 +63,29 @@ const Home = () => {
 }
 
 const Container = styled.View`
-  background-color: red;
+  background-color: ${props => props.theme.darkGreyColor};
   border-radius: 16px;
   height: 50%;
   margin: 4%;
 `
-const Test = styled.View`
+const Content = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: center;
   padding-top: 25%;
 `
-
-const Content = styled.View`
-  background-color: pink;
+const Box = styled.View`
+  background-color: ${props => props.theme.greyColor};
   border-radius: 10px;
   height: 80px;
   margin: 6px;
   width: 26%;
 `
+const Title = styled.Text`
+  color: ${props => props.theme.lightGreyColor};
+  text-align: center;
+  font-size: 10px;
+`
+
 
 export default Home;
