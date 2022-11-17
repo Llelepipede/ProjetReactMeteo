9671e196435b01ab3322d83dcbb8146aad9d12da
Leolas64 <ellande.villemain@hotmail.fr>
2022-11-17 14:47:53 +0100

    Maj Theme

---
+++ src/configuration/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+
+  // Color Palette
+
+  // DARK MODE
+  blackColor: '#000000',
+  darkGreyColor: '#0B0F1A',
+  greyColor: '#101423',
+  lightGreyColor: '#CCD1E6',
+  pinkColor: '#EF8AB2',
+
+  // LIGHT MODE
+  whiteColor: '#FFFFFF',
+
+}
+
+export default theme;
