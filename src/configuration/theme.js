2332b2c659e84618c57698b4c20943dd85895e43
Leolas64 <ellande.villemain@hotmail.fr>
2022-11-17 15:59:01 +0100

    Dark theme / Light theme

--- src/configuration/theme.js
+++ src/configuration/theme.js
@@ -1,17 +1,17 @@
-const theme = {
-
-  // Color Palette
-
-  // DARK MODE
-  blackColor: '#000000',
-  darkGreyColor: '#0B0F1A',
-  greyColor: '#101423',
-  lightGreyColor: '#CCD1E6',
-  pinkColor: '#EF8AB2',
-
-  // LIGHT MODE
-  whiteColor: '#FFFFFF',
-
-}
-
 export default theme;
+
+const darkTheme = {
+  PRIMARY_COLOR: '#000000', // Black
+  SECONDARY_COLOR: '#444444', // Dark Grey
+  TITLE_COLOR: '#999999', // Grey
+  BACKGROUND_COLOR: '#EEEEEE', // Light Grey
+  BUTTON_COLOR: '#EF8AB2', // Pink
+};
+const lightTheme = {
+  PRIMARY_COLOR: '#FFFFFF', //White
+  SECONDARY_COLOR: '#16537E', // Dark Blue
+  TITLE_COLOR: '#6FA8DC', // Blue
+  BACKGROUND_COLOR: '#CFE2F3', // Light Blue
+  BUTTON_COLOR: '#EF8AB2', // Pink
+};
+export { lightTheme, darkTheme };
