132ed2a4d06d69a45a5c818a0541c0079aa319f4
hakimbg1 <74165701+hakimbg1@users.noreply.github.com>
2022-11-21 22:14:48 +0100

    add icons

--- src/configuration/bottomNavigator.js
+++ src/configuration/bottomNavigator.js
@@ -4,18 +4,46 @@ import { View, Text } from "react-native";
 
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
+import Icon from 'react-native-vector-icons/Ionicons'
+
+
+// Screens
 import Favorite from '../screens/Favorite';
+import Home from '../screens/Home';
 import Position from '../screens/Position';
-import HomeStack from './homeStack';
+
+
+//Screen Names
+const favoriName = 'Favorite';
+const homeName = 'Home';
+const positionName = 'Position';
 
 const BottomNavigator = () => {
+  
   const BottomStack = createMaterialBottomTabNavigator();
 
   return (
-    <BottomStack.Navigator>
-      <BottomStack.Screen name="Position" component={Position} />
-      <BottomStack.Screen name="HomeStack" component={HomeStack} />
-      <BottomStack.Screen name="Favorite" component={Favorite} />
+    <BottomStack.Navigator initialRouteName={homeName}
+    screenOptions = {({route}) => ({
+      headerShown : false,
+      tabBarIcon : ({focused, color , size}) =>{
+        let iconName;
+
+        if (route.name === homeName) {
+          iconName = focused ? 'albums' : 'albums-outline';
+        }else if (route.name === favoriName) {
+            iconName = focused ? 'albums' : 'albums-outline';
+        }else if (route.name === positionName) {
+          iconName = focused ? 'albums' : 'albums-outline';
+        }
+          return  <Icon name={iconName} size={size} color={color} />
+        },
+    })}
+    >
+        
+      <BottomStack.Screen name={positionName} component={Position} /> 
+      <BottomStack.Screen name={homeName} component={Home} />
+      <BottomStack.Screen name={favoriName} component={Favorite} />
     </BottomStack.Navigator >
   );
 }
