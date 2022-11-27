b6afba235b4499fe975303d9501161c431bb4789
Delphine <delphine.pva@gmail.com>
2022-11-27 17:00:21 +0100

    Map View installed

--- src/screens/Position/index.js
+++ src/screens/Position/index.js
@@ -1,15 +1,80 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
 
-import GetLocation from '../../components/GetLocation';
+// import GetLocation from '../../components/GetLocation';
+
+// const Position = () => {
+//   useEffect(() => {
+//     console.log(GetLocation());
+//   });
+
+//   return <GetLocation />
+// };
+
+// export default Position;
+
+import React, { useState } from 'react';
+
+import MapView, { Callout, Circle, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 const Position = () => {
-  useEffect(() => {
-    console.log(GetLocation());
-  });
+  const [pin, setPin] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324
+  })
 
-  return <GetLocation />
-};
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      // provider={PROVIDER_GOOGLE}
+      >
+        <Marker
+          coordinate={pin}
+          pinColor="black"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log('Drag start', e.nativeEvent.coordinates)
+          }}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude
+            })
+          }}
+        >
+          <Callout>
+            <Text>I'm here</Text>
+          </Callout>
+        </Marker>
+        <Circle center={pin}
+          radius={1000}
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  conainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+});
 
 export default Position;
